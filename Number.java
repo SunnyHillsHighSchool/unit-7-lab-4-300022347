a1d3369b110b7cd679a1b7550ffd786511a74235
Kyle Kim <300022347@fjuhsd.org>
2021-02-13 04:32:40 +0000

    Completed unit 7 lab 4

--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Kyle Kim
 
 
 //write the Number class
@@ -9,20 +9,79 @@
 //to guide you
 
 //header for Number class
-
+public class Number
+{
 //private instance variable (must be an Integer)
-
+private Integer num;
 //constructor
+public Number()
+{
+  num = 0;
+}
 
-
+public Number(Integer initNum)
+{
+  num = initNum;
+}
 //getter method
-
+public Integer getNum()
+{
+  return num;
+}
 
 //setter method
-
+public void setNum(int n)
+{
+  num = n;
+}
 //isOdd method
 
+public void isOdd(int p)
+{
+  boolean odd = false;
+
+  if((p%2)==0)
+  {
+    odd = false;
+  }
+
+  else
+  {
+    odd = true;
+  }
+
+}
+
+
+
+
+
+
 //isPerfect method
+public void isPerfect(int a)
+{
+  int total = 0;
+  boolean perfect = false;
 
+  for(int i=0; i<(a-1);i++)
+  {
+    if((a%i)==0)
+    {
+      total+=i;
+    }
 
+  }
+
+  if(total==a)
+  {
+    perfect = true;
+  }
+
+  else
+  {
+    perfect = false;
+  }
+}
+
+}
 //toString
